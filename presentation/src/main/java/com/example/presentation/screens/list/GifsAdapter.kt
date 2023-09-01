6d675131811492cd14953a6638e79e1db8bd6700
anastasiia1410 <ar.galkina99@gmail.com>
2023-09-01 12:47:05 +0300

    add progress bar while loading

--- presentation/src/main/java/com/example/presentation/screens/list/GifsAdapter.kt
+++ presentation/src/main/java/com/example/presentation/screens/list/GifsAdapter.kt
@@ -1,12 +1,18 @@
 package com.example.presentation.screens.list
 
 import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.domain.entity.Gif
 import com.example.presentation.databinding.ItemGifBinding
-import com.example.presentation.utils.loadImage
 
 class GifsAdapter : RecyclerView.Adapter<GifsAdapter.VH>() {
     private var items: MutableList<Gif> = mutableListOf()
@@ -22,10 +28,36 @@ class GifsAdapter : RecyclerView.Adapter<GifsAdapter.VH>() {
         val item = items[position]
         with(holder.binding)
         {
-            ivImage.loadImage(item.image.fixedHeightSmallUrl.url)
+            pbProgressBar.visibility = View.VISIBLE
+            Glide.with(ivImage.context)
+                .load(item.image.fixedHeightSmallUrl.url)
+                .fitCenter()
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean,
+                    ): Boolean {
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean,
+                    ): Boolean {
+                        pbProgressBar.visibility = View.GONE
+                        return false
+                    }
+                })
+                .into(ivImage)
             ivImage.setOnClickListener { onGifClick?.invoke(item.image.originalUrl.url) }
         }
     }
+
     @SuppressLint("NotifyDataSetChanged")
     fun updateItems(newItems: List<Gif>) {
         items.clear()
